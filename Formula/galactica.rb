816452b7f780d41b5a8015c5945eb9db11359580
Tom Wells <drshade@gmail.com>
2023-03-07 15:36:26 +0100

    hotfix of login

--- Formula/galactica.rb
+++ Formula/galactica.rb
@@ -3,7 +3,7 @@ class Galactica < Formula
 	desc "Galactica GPT3.5 swiss army knife"
 	homepage "https://github.com/synthesis-labs/galactica-cli"
 	url "https://github.com/synthesis-labs/galactica-cli/releases/download/beta-rc.1/galactica-x86_64-apple-darwin.tar.gz"
-	sha256 "78481d6807a5b00947f258a0af2ef721a5e926845aa308634c0c0a38dac7de54"
+	sha256 "e77c4667cbb168b88b091d64c15563614503258ab102b1ca5f625f3b3cd02095"
 	version "beta-rc.1"
   
 	def install
