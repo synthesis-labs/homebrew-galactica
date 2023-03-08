3e1e08281a51c2ec11ddb08868d2db2e47779d2e
Tom Wells <drshade@gmail.com>
2023-03-08 13:33:59 +0100

    another bump

--- Formula/galactica.rb
+++ Formula/galactica.rb
@@ -3,8 +3,8 @@ class Galactica < Formula
 	desc "Galactica GPT3.5 swiss army knife"
 	homepage "https://github.com/synthesis-labs/galactica-cli"
 	url "https://github.com/synthesis-labs/galactica-cli/releases/download/beta-rc.1/galactica-x86_64-apple-darwin.tar.gz"
-	sha256 "385fcb6d24bea6d09a446602270615c6fa679038d31e85be4b1255067f18eeaf"
-	version "beta-rc.1b"
+	sha256 "4b2e8d2dd547abf4ead376a39eed7f2673e5b3d84a5d684bf798ae8092184c02"
+	version "beta-rc.1c"
   
 	def install
 	  bin.install "galactica"
