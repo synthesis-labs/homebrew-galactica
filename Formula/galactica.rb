f86492709a1c8c115856eedb20864a3ddde5df8b
Michael Grant <mikeg@synthesis.co.za>
2023-03-09 10:27:42 +0200

    Update pointing to aarch tar

--- Formula/galactica.rb
+++ Formula/galactica.rb
@@ -2,7 +2,7 @@
 class Galactica < Formula
 	desc "Galactica GPT3.5 swiss army knife"
 	homepage "https://github.com/synthesis-labs/galactica-cli"
-	url "https://github.com/synthesis-labs/galactica-cli/releases/download/beta-rc.1/galactica-x86_64-apple-darwin.tar.gz"
+	url "https://github.com/synthesis-labs/galactica-cli/releases/download/beta-rc.1/galactica-aarch64-apple-darwin-0.1.0-build.17.014b5bf.tar.gz"
 	sha256 "4b2e8d2dd547abf4ead376a39eed7f2673e5b3d84a5d684bf798ae8092184c02"
 	version "beta-rc.1c"
   
