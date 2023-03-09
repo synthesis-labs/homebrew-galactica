db0d42976802fd5a47aa386ff9212b407da324ca
Michael Grant <mikeg@synthesis.co.za>
2023-03-09 10:30:43 +0200

    updated checksum

--- Formula/galactica.rb
+++ Formula/galactica.rb
@@ -3,7 +3,7 @@ class Galactica < Formula
 	desc "Galactica GPT3.5 swiss army knife"
 	homepage "https://github.com/synthesis-labs/galactica-cli"
 	url "https://github.com/synthesis-labs/galactica-cli/releases/download/beta-rc.1/galactica-aarch64-apple-darwin-0.1.0-build.17.014b5bf.tar.gz"
-	sha256 "4b2e8d2dd547abf4ead376a39eed7f2673e5b3d84a5d684bf798ae8092184c02"
+	sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
 	version "beta-rc.1c"
   
 	def install
