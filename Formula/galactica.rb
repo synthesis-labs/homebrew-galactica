696c80b708dc00c5864bbcf3690e49eff0251405
Tom Wells <drshade@gmail.com>
2023-03-09 12:11:37 +0100

    fix

--- Formula/galactica.rb
+++ Formula/galactica.rb
@@ -4,11 +4,11 @@ class Galactica < Formula
 	homepage "https://github.com/synthesis-labs/galactica-cli"
 
 	on_arm do
-		url = "https://github.com/synthesis-labs/galactica-cli/releases/download/beta-rc.1/galactica-x86_64-apple-darwin-0.1.0+build.23.f5a12e2.tar.gz"
+		url "https://github.com/synthesis-labs/galactica-cli/releases/download/beta-rc.1/galactica-x86_64-apple-darwin-0.1.0+build.23.f5a12e2.tar.gz"
 		sha256 "d37756338aea7021b21f1c50adea183a4148b37a3b60c040b241be789da2bfb3"
 	end
 	on_intel do
-		url = "https://github.com/synthesis-labs/galactica-cli/releases/download/beta-rc.1/galactica-aarch64-apple-darwin-0.1.0+build.23.f5a12e2.tar.gz"
+		url "https://github.com/synthesis-labs/galactica-cli/releases/download/beta-rc.1/galactica-aarch64-apple-darwin-0.1.0+build.23.f5a12e2.tar.gz"
 		sha256 "fb4286df97b670502c865aa0f4f607a35058186051fe56e9812932bce6882130"
 	end
 
