e1f7c128443dc6cb44720f902c6dc5084a6f4db4
Tom Wells <drshade@gmail.com>
2023-03-09 12:02:36 +0100

    bump

--- Formula/galactica.rb
+++ Formula/galactica.rb
@@ -3,15 +3,15 @@ class Galactica < Formula
 	desc "Galactica GPT3.5 swiss army knife"
 	homepage "https://github.com/synthesis-labs/galactica-cli"
 	on_arm do
-		url "https://github.com/synthesis-labs/galactica-cli/releases/download/beta-rc.1/galactica-aarch64-apple-darwin-0.1.0-build.17.014b5bf.tar.gz"
-		sha256 "40151b8ea6bc7e119046f5dd282830188bb14124766e1f9558f59628c210b08e"
+		url "https://github.com/synthesis-labs/galactica-cli/releases/download/beta-rc.1/galactica-x86_64-apple-darwin-0.1.0+build.23.f5a12e2.tar.gz"
+		sha256 "d37756338aea7021b21f1c50adea183a4148b37a3b60c040b241be789da2bfb3"
 	end
 	on_intel do
-		url "https://github.com/synthesis-labs/galactica-cli/releases/download/beta-rc.1/galactica-x86_64-apple-darwin-0.1.0-build.17.014b5bf.tar.gz"
-		sha256 "2b9718be22f264ea0889e854cd2b85d1f8bbeed2040132bdcd19f50e0fc77a7c"
+		url "https://github.com/synthesis-labs/galactica-cli/releases/download/beta-rc.1/galactica-aarch64-apple-darwin-0.1.0+build.23.f5a12e2.tar.gz"
+		sha256 "fb4286df97b670502c865aa0f4f607a35058186051fe56e9812932bce6882130"
 
 	end
-	version "beta-rc.1d"
+	version "beta-rc.1e"
   
 	def install
 	  bin.install "galactica"
