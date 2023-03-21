b9a77aa3816c55f3dbf2150f9b3efe42fbfa536e
Tom Wells <drshade@gmail.com>
2023-03-21 15:08:40 +0100

    bump new version

--- Formula/galactica.rb
+++ Formula/galactica.rb
@@ -6,7 +6,7 @@ class Galactica < Formula
 	url "https://github.com/synthesis-labs/galactica-cli/releases/download/0.1.0/galactica-x86_64-apple-darwin-0.1.0+build.93.c770688.zip"
 	sha256 "0c123723dae856cbc5f55c52488f74e2629bc588b4762c5e56a1a9461414636e"
 
-	version "beta-rc.1i"
+	version "0.1.0"
   
 	def install
 	  bin.install "galactica"
