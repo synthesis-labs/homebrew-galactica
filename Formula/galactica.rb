ba81f74d361043821a1b43bbdb7553304e32f8c5
Tom Wells <drshade@gmail.com>
2023-03-21 15:06:12 +0100

    bumping version to new scheme shortly

--- Formula/galactica.rb
+++ Formula/galactica.rb
@@ -4,15 +4,11 @@ class Galactica < Formula
 	homepage "https://github.com/synthesis-labs/galactica-cli"
 
 	on_intel do
-		url "https://github.com/synthesis-labs/galactica-cli/releases/download/beta-rc.1/galactica-x86_64-apple-darwin-0.1.0+build.53.ef9a0f8.tar.gz"
-		sha256 "87db8ef66585c1910e7dfcd8548392a1880bdb854b2d629018ad88e5507f42d7"
-	end
-	on_arm do
-		url "https://github.com/synthesis-labs/galactica-cli/releases/download/beta-rc.1/galactica-aarch64-apple-darwin-0.1.0+build.53.ef9a0f8.tar.gz"
-		sha256 "31d232ec6fa6d8654b797c5836b28274616762450a6194684d295126e7b25d54"
+		url "https://github.com/synthesis-labs/galactica-cli/releases/download/0.1.0/galactica-x86_64-apple-darwin-0.1.0+build.93.c770688.zip"
+		sha256 "0c123723dae856cbc5f55c52488f74e2629bc588b4762c5e56a1a9461414636e"
 	end
 
-	version "beta-rc.1h"
+	version "beta-rc.1i"
   
 	def install
 	  bin.install "galactica"
