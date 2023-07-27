e9861a105a1455733e850362392c10cf732b97ea
Tom Wells <drshade@gmail.com>
2023-07-27 17:06:29 +0200

    bump homebrew version

--- Formula/galactica.rb
+++ Formula/galactica.rb
@@ -3,10 +3,10 @@ class Galactica < Formula
 	desc "GalacticAI AI swiss army knife"
 	homepage "https://github.com/synthesis-labs/galactica-cli"
 
-	url "https://github.com/synthesis-labs/galactica-cli/releases/download/0.1.0/galactica-x86_64-apple-darwin-0.1.0+build.107.e3e309d.zip"
-	sha256 "ff948c2be42444ba893547b333f360f32c2334151714b29a0a14f0f5a74597f8"
+	url "https://github.com/synthesis-labs/galactica-cli/releases/download/0.1.0/galactica-x86_64-apple-darwin-0.1.0+build.108.3de9b15.zip"
+	sha256 "692d3cb88e94d47ed7499050a21f9df0f54d28d11e5b56d0cc5430d616eef773"
 
-	version "0.1.0f"
+	version "0.1.0g"
   
 	def install
 	  bin.install "galactica"
